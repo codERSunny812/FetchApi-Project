6c69826ec6115010f704744cdff7b6c785254173
sunny <sengersunny448@gmail.com>
2023-06-29 12:39:08 +0530

    js file updated

--- Country Guide App/script.js
+++ Country Guide App/script.js
@@ -1,96 +1,81 @@
 let btn= document.getElementById('Submit-btn');
 const inputCountry=document.getElementById('country-name');
 const resultCont=document.getElementById('result');
-
-
-
-
 btn.addEventListener("click",()=>{
-    console.log("im clicked ");
     let countryName=inputCountry.value;
-    console.log(countryName);
-    let finalUrl=`https://restcountries.com/v3.1/name/${countryName}`;
-    fetch(finalUrl)
-        .then((response)=>{
-        return response.json();
-    }).then((data)=>{
-        // console.log(data[0]);
-        // console.log(data[0].capital[0]);
-        // console.log(data[0].flags.svg);
-        // console.log(data[0].name.common);
-        // console.log(data[0].continents[0]);
-        // console.log(data[0].timezones[0]);
-        // console.log(data[0].population);
-        // console.log(data[0].currencies.INR.name);
-        // console.log(data[0].area);
-        // console.log(data[0].maps.googleMaps);
+    let Url=`https://restcountries.com/v3.1/name/${countryName}`;
+
+    async function getData(){
+        let data = await fetch(Url);
+        let value = await data.json();
+        console.log(value[0]);
+        console.log(value[0]?.name?.common);
+        console.log(value[0]?.capital[0]);
+        console.log(value[0]?.continents[0]);
+        console.log(value[0]?.flags?.svg);
+        console.log(value[0]?.timezones[0]);
+        console.log(value[0]?.population);
+        console.log(value[0]?.currencies);
+        console.log(value[0]?.area);
+        console.log(value[0]?.maps?.googleMaps);
+
         resultCont.innerHTML=`
+          <img src=${value[0]?.flags?.svg} class="flag-image" >
+            <h2>${value[0]?.name?.common}</h2>
+           <div id="wrapper"> 
+            <div id="data-wrapper">
+            <h4>continents:</h4>
+           <span>${value[0]?.continents[0]}</span>
+           </div>
+           </div>
+           <div id="wrapper"> 
+           <div id="data-wrapper">
+           <h4>capital:</h4>
+           <span>${value[0]?.capital[0]}</span>
+           </div>
+           </div>
+           <div id="wrapper"> 
+           <div id="data-wrapper">
+           <h4>timezone:</h4>
+           <span>${value[0]?.timezones[0]}</span>
+           </div>
+           </div>
+           <div id="wrapper"> 
+           <div id="data-wrapper">
+           <h4>population:</h4>
+           <span>${value[0]?.population}</span>
+           </div>
+           </div>
+           <div id="wrapper"> 
+           <div id="data-wrapper">
+           <h4>currencies:</h4>
+           <span>${value[0]?.currencies[Object.keys(value[0]?.currencies)].name} - ${Object.keys(value[0]?.currencies)}</span>
+           </div>
+           </div>
+           <div id="wrapper"> 
+           <div id="data-wrapper">
+           <h4>total area :</h4>
+           <span>${value[0]?.area}</span>
+           </div>
+           </div>
+           <div id="wrapper"> 
+           <div id="data-wrapper">
+           <h4>map link:</h4>
+           <span>${value[0]?.maps?.googleMaps} </span>
+           </div>
+           </div>` 
 
+    }
+    getData();
+    
+})
         
-        <img src=${data[0].flags.svg} class="flag-image" >
-        
-        <h2>${data[0].name.common}</h2>
        
-        <div id="wrapper"> 
-        <div id="data-wrapper">
-        <h4>continents:</h4>
-        <span>${data[0].continents[0]}</span>
-        </div>
-        </div>
-
-        <div id="wrapper"> 
-        <div id="data-wrapper">
-        <h4>capital:</h4>
-        <span>${data[0].capital[0]}</span>
-        </div>
-        </div>
-
-        <div id="wrapper"> 
-        <div id="data-wrapper">
-        <h4>timezone:</h4>
-        <span>${data[0].timezones[0]}</span>
-        </div>
-        </div>
-        
-        <div id="wrapper"> 
-        <div id="data-wrapper">
-        <h4>population:</h4>
-        <span>${data[0].population}</span>
-        </div>
-        </div>
-
-         
-        <div id="wrapper"> 
-        <div id="data-wrapper">
-        <h4>currencies:</h4>
-        <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)}</span>
-        </div>
-        </div>
-
-        
-        <div id="wrapper"> 
-        <div id="data-wrapper">
-        <h4>total area :</h4>
-        <span>${data[0].area}</span>
-        </div>
-        </div>
-
+   
+    
        
-        <div id="wrapper"> 
-        <div id="data-wrapper">
-        <h4>map link:</h4>
-        <span>${data[0].maps.googleMaps} </span>
-        </div>
-        </div>
-
-
-        `
-         
-        ;
-        
-    });
 
   
-});
+
 
 
