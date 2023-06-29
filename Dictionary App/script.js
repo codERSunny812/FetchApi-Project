eea48a3b661e433ea31847a5a887d91c2ff44b0f
sunny <sengersunny448@gmail.com>
2023-06-30 00:30:36 +0530

    dictionary app

--- Dictionary App/script.js
+++ Dictionary App/script.js
@@ -1,40 +1,75 @@
 const url="https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result= document.getElementById("result");
-const btn=document.getElementById('Submit-btn');
-
-
+const btn=document.getElementById('submit-btn');
 
 btn.addEventListener(
-    'click',
+    "click",
     ()=>{
+
         let inputWord=document.getElementById("word-search").value;
         console.log(inputWord);
-       fetch(`${url}${inputWord}`)
-       .then((resp)=>{
-        resp.json();
-       }).then((data)=>{
-        console.log(data);
-    })
 
-    result.innerHTML=`
-    <div class="word">
-     <h2>${inputWord}</h2>
-     <button>
-         icon
-     </button>
-    </div>
+        async  function getData(){
+            let data= await fetch(`${url}${inputWord}`);
+            let jsonData= await data.json();
+            console.log(jsonData);
+            console.log(jsonData[0].word);
+            console.log(jsonData[0].meanings[0].partOfSpeech);
+            console.log(jsonData[0].phonetics[0].text);
+            console.log(jsonData[0].meanings[0].definitions[0].definition);
+            console.log(jsonData[0].meanings[1].synonyms);
+            console.log(jsonData[0].meanings[1].definitions[0].definition);
+            console.log(jsonData[0].meanings[2].definitions[0].definition);
+            result.innerHTML=`
+            <div class="word">
+             <h2>${jsonData[0].word}</h2>
+            </div>
+            
+            <div class="details">
+             <p>${jsonData[0].meanings[0].partOfSpeech}</p>
+             <p>/${jsonData[0].phonetics[0].text}/</p>
+            </div>
+            <p className="defination">
+            ${jsonData[0].meanings[0].definitions[0].definition}
+            </p>
+            <p className="synonyms">
+            ${jsonData[0].meanings[1].synonyms}
+            </p>
+            <p class="word-meaning">
+            ${jsonData[0].meanings[1].definitions[0].definition}
+            </p>
+            <p class="word-example">
+            ${jsonData[0].meanings[2].definitions[0].definition} 
+             </p>
+            `;
+
+
+        }
+        getData();
+        
+        
+
+    }
+)
+
+
+
+
+
+
+
+       
+      
+
+      
+
+
+
+ 
+
     
-    <div class="details">
-     <p></p>
-     <p>/sample/</p>
-    </div>
-    <p class="word-meaning">
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, vero dolore. Veritatis.
-    </p>
-    <p class="word-example">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, deserunt.  
-    // </p>
-    `;
 
-});
+    
+
+
 
